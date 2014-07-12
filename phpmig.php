891b90062ea0356dea36bfb54c22b0823a0c383e
Kaito Udagawa <umireon@gmail.com>
2014-07-12 11:26:58 +0900

    Migrate CreateTacks with phpmig.

---
+++ phpmig.php
@@ -0,0 +1,25 @@
+<?php
+
+use \Phpmig\Adapter;
+use \Pimple;
+use \Doctrine\DBAL\DriverManager;
+
+$container = new Pimple();
+
+$container['db.dsn'] = getenv('DATABASE_DSN');
+
+$container['db'] = function($c) {
+    $dbh = new PDO($c['db.dsn']);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return DriverManager::getConnection(array(
+        'pdo' => $dbh
+    ));
+};
+
+$container['phpmig.adapter'] = function ($c) {
+    return new Adapter\Doctrine\DBAL($c['db'], 'migrations');
+};
+
+$container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
+
+return $container;
