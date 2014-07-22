55130574c2c1e118e70916f630316b2e092feaa7
Kaito Udagawa <umireon@gmail.com>
2014-07-23 00:34:04 +0900

    Controller Tests For Tacks#index and #show

--- application/classes/controller/tacks.php
+++ application/classes/controller/tacks.php
@@ -1,19 +1,41 @@
 <?php
 
 class Controller_Tacks extends Controller {
-	protected function newModelTack($id = NULL)
+	private $factory;
+
+	public function before()
 	{
-		return new Model_Tack($id);
+		parent::before();
+		if (empty($this->factory)) {
+			$this->factory = new Factory;
+		}
+	}
+
+	final public function set_factory(Factory $factory)
+	{
+		$this->factory = $factory;
 	}
 
 	public function action_index()
 	{
-		$tacks = $this->newModelTack()->find_all();
-		$this->response->body(var_export($tacks->as_array(), TRUE));
+		$tacks = $this->factory->model('tack')->find_all();
+		$view = $this->factory->view('tacks/index', array('tacks' => $tacks));
+		$this->response->body($view);
 	}
 
 	public function action_show()
 	{
+		$req = $this->request;
+
+		$year = $req->param('year');
+		$month = $req->param('month');
+		$day = $req->param('day');
+		$title = $req->param('title');
+
+		$model = $this->factory->model('tack');
+		$tack = $model->find_by_date_and_title($year, $month, $day, $title);
+		$view = $this->factory->view('tacks/show', array('tack' => $tack));
+		$this->response->body($view);
 	}
 
 	public function action_add()
--- application/tests/controller/TacksTest.php
+++ application/tests/controller/TacksTest.php
@@ -8,13 +8,73 @@ require_once 'application/tests/DBTestCase.php';
  * @copyright  Copyright (c) 2014 Kaito Udagawa
  * @license    http://opensource.org/licenses/MIT
  */
-class Controller_TackTest extends DBTestCase
+class Controller_TacksTest extends DBTestCase
 {
 	protected $yaml = 'application/tests/fixture/tacks.yml';
 
-	public function testRouteIndex()
+	public function testIndex()
 	{
-		$response = Request::factory('tacks')->execute();
-		$this->assertContains('title1', $response->body());
+		$model = $this->getMock('Model_Tack');
+		$model->expects($this->once())
+		      ->method('find_all')
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock('Factory');
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+		$factory->expects($this->once())
+		        ->method('view')
+		        ->with($this->identicalTo('tacks/index'),
+		               $this->identicalTo(array('tacks' => $model)))
+		        ->will($this->returnValue('tacks/index'));
+
+		$req = new Request('');
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_index();
+		// end code
+
+		$this->assertContains('tacks/index', $res->body());
+	}
+
+	public function testShow()
+	{
+		$model = $this->getMock('Model_Tack');
+		$model->expects($this->once())
+		      ->method('find_by_date_and_title')
+		      ->with($this->equalTo('2000'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('title'))
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock('Factory');
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+		$factory->expects($this->once())
+		        ->method('view')
+		        ->with($this->identicalTo('tacks/show'),
+		               $this->identicalTo(array('tack' => $model)))
+		        ->will($this->returnValue('tacks/show'));
+
+		$req = new Request('tacks/2000/01/01/title', NULL, array(
+			'injected' => new Route('<controller>/<year>/<month>/<day>/<title>'),
+		));
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_show();
+		// end code
+
+		$this->assertContains('tacks/show', $res->body());
 	}
 } // End TackTest
