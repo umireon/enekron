ba68a60ebe3db9dbf5afd00d58e4169a6a6a3af3
Kaito Udagawa <umireon@gmail.com>
2014-07-22 15:29:59 +0900

    Easy to unittest with controller by obtaining the model object from controller-specific factory method.

--- application/classes/controller/tacks.php
+++ application/classes/controller/tacks.php
@@ -1,10 +1,14 @@
 <?php
 
 class Controller_Tacks extends Controller {
+	protected function newModelTack($id = NULL)
+	{
+		return new Model_Tack($id);
+	}
 
 	public function action_index()
 	{
-		$tacks = ORM::factory('tack')->find_all();
+		$tacks = $this->newModelTack()->find_all();
 		$this->response->body(var_export($tacks->as_array(), TRUE));
 	}
 
