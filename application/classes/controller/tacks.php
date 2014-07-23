16facc142dc44bc4f25bebf531ca3a1d34e1aee8
Kaito Udagawa <umireon@gmail.com>
2014-07-23 13:21:26 +0900

    Controller Tests for tacks#add, tacks#edit and tacks#delete

--- application/classes/controller/tacks.php
+++ application/classes/controller/tacks.php
@@ -26,7 +26,6 @@ class Controller_Tacks extends Controller {
 	public function action_show()
 	{
 		$req = $this->request;
-
 		$year = $req->param('year');
 		$month = $req->param('month');
 		$day = $req->param('day');
@@ -40,14 +39,59 @@ class Controller_Tacks extends Controller {
 
 	public function action_add()
 	{
+		$req = $this->request;
+
+		if ($req->method() === 'POST') {
+			$model = $this->factory->model('tack');
+			$model->title = $req->post('title');
+			$model->content = $req->post('content');
+			$model->create();
+		} else {
+			$view = $this->factory->view('tacks/add');
+			$this->response->body($view);
+		}
 	}
 
 	public function action_edit()
 	{
+		$req = $this->request;
+
+		$year = $req->param('year');
+		$month = $req->param('month');
+		$day = $req->param('day');
+		$title = $req->param('title');
+
+		$model = $this->factory->model('tack');
+		$tack = $model->find_by_date_and_title($year, $month, $day, $title);
+
+		if ($req->method() === 'POST') {
+			$tack->title = $req->post('title');
+			$tack->content = $req->post('content');
+			$tack->update();
+		} else {
+			$view = $this->factory->view('tacks/edit', array('tack' => $tack));
+			$this->response->body($view);
+		}
 	}
 
 	public function action_delete()
 	{
+		$req = $this->request;
+
+		$year = $req->param('year');
+		$month = $req->param('month');
+		$day = $req->param('day');
+		$title = $req->param('title');
+
+		$model = $this->factory->model('tack');
+		$tack = $model->find_by_date_and_title($year, $month, $day, $title);
+
+		if ($req->method() === 'POST') {
+			$tack->delete();
+		} else {
+			$view = $this->factory->view('tacks/delete', array('tack' => $tack));
+			$this->response->body($view);
+		}
 	}
 
 } // End Tacks
--- application/tests/controller/TacksTest.php
+++ application/tests/controller/TacksTest.php
@@ -8,18 +8,19 @@ require_once 'application/tests/DBTestCase.php';
  * @copyright  Copyright (c) 2014 Kaito Udagawa
  * @license    http://opensource.org/licenses/MIT
  */
-class Controller_TacksTest extends DBTestCase
+class Controller_TacksTest extends PHPUnit_Framework_TestCase
 {
-	protected $yaml = 'application/tests/fixture/tacks.yml';
+	public static $model = 'Model_Tack';
+	public static $factory = 'Factory';
 
 	public function testIndex()
 	{
-		$model = $this->getMock('Model_Tack');
+		$model = $this->getMock(self::$model);
 		$model->expects($this->once())
 		      ->method('find_all')
 		      ->will($this->returnSelf());
 
-		$factory = $this->getMock('Factory');
+		$factory = $this->getMock(self::$factory);
 		$factory->expects($this->once())
 		        ->method('model')
 		        ->with($this->identicalTo('tack'))
@@ -44,7 +45,7 @@ class Controller_TacksTest extends DBTestCase
 
 	public function testShow()
 	{
-		$model = $this->getMock('Model_Tack');
+		$model = $this->getMock(self::$model);
 		$model->expects($this->once())
 		      ->method('find_by_date_and_title')
 		      ->with($this->equalTo('2000'),
@@ -53,7 +54,7 @@ class Controller_TacksTest extends DBTestCase
 		      	     $this->equalTo('title'))
 		      ->will($this->returnSelf());
 
-		$factory = $this->getMock('Factory');
+		$factory = $this->getMock(self::$factory);
 		$factory->expects($this->once())
 		        ->method('model')
 		        ->with($this->identicalTo('tack'))
@@ -76,5 +77,202 @@ class Controller_TacksTest extends DBTestCase
 		// end code
 
 		$this->assertContains('tacks/show', $res->body());
+	}
+
+	public function testAddGet()
+	{
+		$factory = $this->getMock(self::$factory);
+		$factory->expects($this->once())
+		        ->method('view')
+		        ->with($this->identicalTo('tacks/add'))
+		        ->will($this->returnValue('tacks/add'));
+
+		$req = new Request('');
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_add();
+		// end code
+	}
+
+	public function testAddPost()
+	{
+		$model = $this->getMock(self::$model, array('create'));
+		$model->expects($this->once())
+		      ->method('create')
+		      ->with()
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock(self::$factory);
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+
+		$req = new Request('');
+		$req->method('POST');
+		$req->post(array(
+			'title' => 'title',
+			'content' => 'content',
+		));
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_add();
+		// end code
+
+		$this->assertSame('title', $model->title);
+		$this->assertSame('content', $model->content);
+	}
+
+	public function testEditGet()
+	{
+		$model = $this->getMock(self::$model);
+		$model->expects($this->once())
+		      ->method('find_by_date_and_title')
+		      ->with($this->equalTo('2000'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('title'))
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock(self::$factory);
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+		$factory->expects($this->once())
+		        ->method('view')
+		        ->with($this->identicalTo('tacks/edit'),
+		               $this->identicalTo(array('tack' => $model)))
+		        ->will($this->returnValue('tacks/edit'));
+
+		$req = new Request('tacks/2000/01/01/title', NULL, array(
+			'injected' => new Route('<controller>/<year>/<month>/<day>/<title>'),
+		));
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_edit();
+		// end code
+
+		$this->assertContains('tacks/edit', $res->body());
+	}
+
+	public function testEditPost()
+	{
+		$model = $this->getMock(self::$model, array('find_by_date_and_title', 'update'));
+		$model->expects($this->once())
+		      ->method('find_by_date_and_title')
+		      ->with($this->equalTo('2000'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('title'))
+		      ->will($this->returnSelf());
+		$model->expects($this->once())
+		      ->method('update')
+		      ->with()
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock(self::$factory);
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+
+		$req = new Request('tacks/2000/01/01/title', NULL, array(
+			'injected' => new Route('<controller>/<year>/<month>/<day>/<title>'),
+		));
+		$req->method('POST');
+		$req->post(array(
+			'title' => 'title',
+			'content' => 'content',
+		));
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_edit();
+		// end code
+
+		$this->assertSame('title', $model->title);
+		$this->assertSame('content', $model->content);
+	}
+
+	public function testDeleteGet()
+	{
+		$model = $this->getMock(self::$model);
+		$model->expects($this->once())
+		      ->method('find_by_date_and_title')
+		      ->with($this->equalTo('2000'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('title'))
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock(self::$factory);
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+		$factory->expects($this->once())
+		        ->method('view')
+		        ->with($this->identicalTo('tacks/delete'),
+		               $this->identicalTo(array('tack' => $model)))
+		        ->will($this->returnValue('tacks/delete'));
+
+		$req = new Request('tacks/2000/01/01/title', NULL, array(
+			'injected' => new Route('<controller>/<year>/<month>/<day>/<title>'),
+		));
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_delete();
+		// end code
+
+		$this->assertContains('tacks/delete', $res->body());
+	}
+
+	public function testDeletePost()
+	{
+		$model = $this->getMock(self::$model, array('find_by_date_and_title', 'delete'));
+		$model->expects($this->once())
+		      ->method('find_by_date_and_title')
+		      ->with($this->equalTo('2000'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('01'),
+		      	     $this->equalTo('title'))
+		      ->will($this->returnSelf());
+		$model->expects($this->once())
+		      ->method('delete')
+		      ->with()
+		      ->will($this->returnSelf());
+
+		$factory = $this->getMock(self::$factory);
+		$factory->expects($this->once())
+		        ->method('model')
+		        ->with($this->identicalTo('tack'))
+		        ->will($this->returnValue($model));
+
+		$req = new Request('tacks/2000/01/01/title', NULL, array(
+			'injected' => new Route('<controller>/<year>/<month>/<day>/<title>'),
+		));
+		$req->method('POST');
+		$res = new Response();
+		$ctrl = new Controller_Tacks($req, $res);
+		$ctrl->set_factory($factory);
+
+		// code
+		$ctrl->action_delete();
+		// end code
 	}
 } // End TackTest
